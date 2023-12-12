1bb0a1bc53d1a2734fb314134b8d3ab39a1d0546
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2023-12-12 15:36:56 +0545

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { NavBar } from "./containers/navbar";
+import { CalenderContextProvider } from "./Context/calender.context";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.Fragment>
+    <CalenderContextProvider>
+      <NavBar />
+    </CalenderContextProvider>
+  </React.Fragment>
+);
